85067009866d64274f6e6667b1ff9bdf988ace88
charles agyemang <micnkru@gmail.com>
2017-05-03 02:10:18 +0000

    aprtment

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -89,5 +89,5 @@ end
 # }
 
 # Rails.application.config.middleware.use 'Apartment::Elevators::Domain'
-Rails.application.config.middleware.use 'Apartment::Elevators::Subdomain'
+#Rails.application.config.middleware.use 'Apartment::Elevators::Subdomain'
 # Rails.application.config.middleware.use 'Apartment::Elevators::FirstSubdomain'
